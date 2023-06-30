7c6ae8219443e4dda0ff66c048261f1c192893bc
nudefingers <nude.fingers@gmail.com>
2023-06-30 20:00:33 +0300

    added controller

--- main.js
+++ main.js
@@ -1,4 +1,50 @@
 const tweeter = Tweeter()
 const renderer = Renderer()
+const posts = tweeter.getPosts()
 
-renderer.renderPosts(tweeter.getPosts())
+renderer.renderPosts(posts)
+
+
+function post() {
+    let newText = $(`#container`).find(`#input`).val()
+    if (newText != ``) {
+        tweeter.addPost(newText)
+        $(`#input`).val(``)
+        renderer.renderPosts(posts)
+    }
+}
+
+
+function deletePost() {
+    let _id = $(this).closest(`.post`).data(`id`)
+
+    tweeter.removePost(_id)
+    renderer.renderPosts(posts)
+}
+
+
+function addComment() {
+    let _text = $(this).closest(`.post`).find(`.comment-input`).val()
+    if (_text != ``) {
+        let _postId = $(this).closest(`.post`).data(`id`)
+
+        tweeter.addComment(_postId, _text)
+        renderer.renderPosts(posts)
+    }
+}
+
+
+function deleteComment() {
+    let _postId = $(this).closest(`.post`).data(`id`)
+    let _commentId = $(this).closest(`.comment`).data(`id`)
+
+    tweeter.removeComment(_postId, _commentId)
+    renderer.renderPosts(posts)
+}
+
+
+
+$(`body`)
+    .on(`click`, `.delete`, deletePost)
+    .on(`click`, `.new-comment`, addComment)
+    .on(`click`, `.delete-comment`, deleteComment)
--- render.js
+++ render.js
@@ -1,24 +1,26 @@
 const Renderer = function () {
 
     function renderPosts(posts) {
-        $("#posts").empty()
+        $(`#posts`).empty()
 
         posts.forEach(function (post) {
             let postDiv = $(`<div>`).addClass(`post`)
             let postText = $(`<div>`).addClass(`post-text`).text(post.text)
-            let deleteButton = $("<button>").addClass("delete").text(`Delete post`)
+            let deletePost = $(`<button>`).addClass(`delete`).text(`Delete post`)
+            let commentInput = $(`<input>`).addClass(`comment-input`).attr(`type`, `text`)
+            let addComment = $(`<button>`).addClass(`new-comment`).text(`Add comment`)
             let commentsDiv = $(`<div>`).addClass(`comments`)
 
             post.comments.forEach(function (comment) {
                 let commentDiv = $(`<div>`).addClass(`comment`).attr(`data-id`, comment.id).text(comment.text)
-                let deleteComment = $(`<span>`).addClass(`delete-comment`).text("\u2715")
+                let deleteComment = $(`<span>`).addClass(`delete-comment`).text(`\u2715`)
 
                 commentDiv.append(deleteComment)
                 commentsDiv.append(commentDiv)
             })
 
             postDiv.attr(`data-id`, post.id)
-            postDiv.append(postText, commentsDiv, deleteButton)
+            postDiv.append(postText, commentsDiv, commentInput, addComment, $(`<br><br>`), deletePost)
             $(`#posts`).append(postDiv)
         })
     }
